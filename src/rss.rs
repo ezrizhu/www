e3d0e4a018c3d3c3ac9d8966ce0d4af5be7ff4d1
Eric Zhu <eric@ericz.me>
2023-07-19 16:35:41 -0400

    refactor

--- src/rss.rs
+++ src/rss.rs
@@ -12,89 +12,89 @@ pub async fn get(State(state): State<super::SiteState>) -> Response {
         .perform_indent(true)
         .create_writer(&mut buf);
 
-    let mut feeds: Vec<XmlEvent> = Vec::new();
-    feeds.push(XmlEvent::StartDocument{
-        version: xml::common::XmlVersion::Version10,
-        encoding: Some("UTF-8"),
-        standalone: Some(true),
-    });
-    feeds.push(XmlEvent::start_element("rss").attr("version", "2.0").into());
-    feeds.push(XmlEvent::start_element("channel").into());
-
-    feeds.push(XmlEvent::start_element("title").into());
-    feeds.push(XmlEvent::characters("Eric's blog").into());
-    feeds.push(XmlEvent::end_element().into());
-
-    feeds.push(XmlEvent::start_element("link").into());
-    feeds.push(XmlEvent::characters("https://ericz.me/blog").into());
-    feeds.push(XmlEvent::end_element().into());
-
-    feeds.push(XmlEvent::start_element("description").into());
-    feeds.push(XmlEvent::characters("Eric's thoughts on things.").into());
-    feeds.push(XmlEvent::end_element().into());
-
-    feeds.push(XmlEvent::start_element("language").into());
-    feeds.push(XmlEvent::characters("en-us").into());
-    feeds.push(XmlEvent::end_element().into());
-
-    feeds.push(XmlEvent::start_element("pubDate").into());
     let pubdate = Utc::now().to_rfc2822();
-    feeds.push(XmlEvent::characters(&pubdate).into());
-    feeds.push(XmlEvent::end_element().into());
+    let feeds: Vec<XmlEvent> = vec![
+        XmlEvent::StartDocument{
+            version: xml::common::XmlVersion::Version10,
+            encoding: Some("UTF-8"),
+            standalone: Some(true),
+        },
+        XmlEvent::start_element("rss").attr("version", "2.0").into(),
+        XmlEvent::start_element("channel").into(),
 
-    feeds.push(XmlEvent::start_element("generator").into());
-    feeds.push(XmlEvent::characters("https://github.com/ericzty/www").into());
-    feeds.push(XmlEvent::end_element().into());
-    
-    feeds.push(XmlEvent::start_element("ttl").into());
-    feeds.push(XmlEvent::characters("1440").into());
-    feeds.push(XmlEvent::end_element().into());
+        XmlEvent::start_element("title").into(),
+        XmlEvent::characters("Eric's blog").into(),
+        XmlEvent::end_element().into(),
+
+        XmlEvent::start_element("link").into(),
+        XmlEvent::characters("https://ericz.me/blog").into(),
+        XmlEvent::end_element().into(),
+
+        XmlEvent::start_element("description").into(),
+        XmlEvent::characters("Eric's thoughts on things.").into(),
+        XmlEvent::end_element().into(),
+
+        XmlEvent::start_element("language").into(),
+        XmlEvent::characters("en-us").into(),
+        XmlEvent::end_element().into(),
+
+        XmlEvent::start_element("pubDate").into(),
+        XmlEvent::characters(&pubdate).into(),
+        XmlEvent::end_element().into(),
+
+        XmlEvent::start_element("generator").into(),
+        XmlEvent::characters("https://github.com/ericzty/www").into(),
+        XmlEvent::end_element().into(),
+
+        XmlEvent::start_element("ttl").into(),
+        XmlEvent::characters("1440").into(),
+        XmlEvent::end_element().into(),
+    ];
 
     for feed in feeds {
         writer.write(feed).unwrap();
     }
 
     for post in state.blog {
-        feeds = Vec::new();
         let title = post.title;
         let link = format!("https://ericz.me/blog/{}", post.slug);
         let date = post.date.to_rfc2822();
         let content = post.body;
 
-        feeds.push(XmlEvent::start_element("item").into());
+        let feeds: Vec<XmlEvent> = vec![
+            XmlEvent::start_element("item").into(),
 
-        feeds.push(XmlEvent::start_element("title").into());
-        feeds.push(XmlEvent::characters(&title).into());
-        feeds.push(XmlEvent::end_element().into());
+            XmlEvent::start_element("title").into(),
+            XmlEvent::characters(&title).into(),
+            XmlEvent::end_element().into(),
 
-        feeds.push(XmlEvent::start_element("guid").into());
-        feeds.push(XmlEvent::characters(&link).into());
-        feeds.push(XmlEvent::end_element().into());
+            XmlEvent::start_element("guid").into(),
+            XmlEvent::characters(&link).into(),
+            XmlEvent::end_element().into(),
 
-        feeds.push(XmlEvent::start_element("link").into());
-        feeds.push(XmlEvent::characters(&link).into());
-        feeds.push(XmlEvent::end_element().into());
+            XmlEvent::start_element("link").into(),
+            XmlEvent::characters(&link).into(),
+            XmlEvent::end_element().into(),
 
-        feeds.push(XmlEvent::start_element("description").into());
-        feeds.push(XmlEvent::cdata(&content).into());
-        feeds.push(XmlEvent::end_element().into());
+            XmlEvent::start_element("description").into(),
+            XmlEvent::cdata(&content).into(),
+            XmlEvent::end_element().into(),
 
-        feeds.push(XmlEvent::start_element("pubDate").into());
-        feeds.push(XmlEvent::characters(&date).into());
-        feeds.push(XmlEvent::end_element().into());
+            XmlEvent::start_element("pubDate").into(),
+            XmlEvent::characters(&date).into(),
+            XmlEvent::end_element().into(),
 
-        feeds.push(XmlEvent::end_element().into());
+            XmlEvent::end_element().into(),
+        ];
         for feed in feeds {
             writer.write(feed).unwrap();
         }
     }
 
-    feeds = Vec::new();
-    feeds.push(XmlEvent::end_element().into());
-    feeds.push(XmlEvent::end_element().into());
-    for feed in feeds {
-        writer.write(feed).unwrap();
-    }
+    let end: XmlEvent = XmlEvent::end_element().into();
+    writer.write(end.clone()).unwrap();
+    writer.write(end).unwrap();
+
     Response::builder()
         .header("Content-Type", "application/rss+xml")
         .body(body::boxed(body::Full::from(buf))).unwrap()
