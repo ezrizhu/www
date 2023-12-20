be19a03c6d86f805ca82e0d919aeaa09dcd3e471
Eric Zhu <eric@ericz.me>
2023-12-20 09:34:47 -0500

    contact update

--- src/site/contact.rs
+++ src/site/contact.rs
@@ -7,20 +7,10 @@ pub async fn contact(State(state): State<super::SiteState>) -> Markup {
     let contact = state.contact.clone();
 
     let content = html! {
-        div class="hero pure-g" {
-            div class="pure-u-1 pure-u-md-1-3 headshot" {
-                img src="/assets/img/eric2.webp" alt="An image of Eric Zhu sitting on a table." class="pure-img headshot";
-                p class="pronouns" { "(they/any)" };
-            }
-            div class="pure-u-1 pure-u-md-2-3" {
-                div class="contact" {
-                    { (PreEscaped(contact)) };
-                }
-            }
-        }
+        { (PreEscaped(contact)) };
     };
     let extra_headers = html! {
-        link rel="stylesheet" href="assets/css/contact.css";
+        link rel="stylesheet" href="assets/css/post.css";
     };
     base("", description, extra_headers, content, Some(state))
 }
