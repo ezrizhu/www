61e115af40487f13e8bdc053dbbab0dab7795229
Ezri Zhu <me@ezrizhu.com>
2024-02-06 18:03:21 -0500

    wkd fix

--- src/pgp.rs
+++ src/pgp.rs
@@ -12,6 +12,6 @@ pub async fn pubkey() -> (HeaderMap, String) {
     let mut resp_header = HeaderMap::new();
     resp_header.insert("Content-Type", "application/octet-stream".parse().unwrap());
     resp_header.insert("Access-Control-Allow-Origin", "*".parse().unwrap());
-    let keyraw = fs::read_to_string("assets/files/publickey.asc").unwrap();
+    let keyraw = fs::read_to_string("assets/files/publickey-binary.asc").unwrap();
     (resp_header, keyraw)
 }
