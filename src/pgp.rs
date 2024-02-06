f65050b5dfd14bb06d71d159a6ae5947424e7b4b
Ezri Zhu <me@ezrizhu.com>
2024-02-06 18:28:25 -0500

    wkd fix

--- src/pgp.rs
+++ src/pgp.rs
@@ -8,10 +8,10 @@ pub async fn policy() -> Html<String> {
     Html(String::from("mailbox-only"))
 }
 
-pub async fn pubkey() -> (HeaderMap, String) {
+pub async fn pubkey() -> (HeaderMap, Vec<u8>) {
     let mut resp_header = HeaderMap::new();
     resp_header.insert("Content-Type", "application/octet-stream".parse().unwrap());
     resp_header.insert("Access-Control-Allow-Origin", "*".parse().unwrap());
-    let keyraw = fs::read_to_string("assets/files/publickey-binary.asc").unwrap();
+    let keyraw = fs::read("assets/files/publickey-binary.asc").unwrap();
     (resp_header, keyraw)
 }
