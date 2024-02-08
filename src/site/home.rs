5a4877f35446fb22e2156b57fb292cdb5cc129a1
Ezri Zhu <me@ezrizhu.com>
2024-02-08 02:05:12 -0500

    update hero pic

--- src/site/home.rs
+++ src/site/home.rs
@@ -15,7 +15,7 @@ pub async fn home(State(state): State<super::SiteState>) -> Markup {
     let content = html! {
         div class="hero pure-g" {
             div class="pure-u-1 pure-u-md-1-3" {
-                img src="/assets/img/ezri1.webp" alt="An image of Ezri Zhu wearing a cow hat." class="pure-img headshot";
+                img src="/assets/img/ezripicrew.webp" alt="An image of Ezri Zhu wearing a cow hat." class="pure-img headshot";
                 p class="pronouns" { "(they/any)" };
             }
             div class="pure-u-1 pure-u-md-2-3" {
